b1f6a17bc5725a8a6b69c0f96e074a8fc080c2a4
pereyralautaro662-bit <pereyralautaro662@gmail.com>
2026-09-24 00:05:55 -0300

    Agrego archivos de and primer ejercicio

---
+++ And primer ejercicio/Form1.Designer.vb
@@ -0,0 +1,161 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.monto = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.cuotas = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.descuento = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.montofinal = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.cuotamensual = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'monto
+        '
+        Me.monto.Location = New System.Drawing.Point(26, 34)
+        Me.monto.Name = "monto"
+        Me.monto.Size = New System.Drawing.Size(208, 20)
+        Me.monto.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(104, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(73, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "ingrese monto"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(104, 57)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "ingrese cuotas"
+        '
+        'cuotas
+        '
+        Me.cuotas.Location = New System.Drawing.Point(38, 73)
+        Me.cuotas.Name = "cuotas"
+        Me.cuotas.Size = New System.Drawing.Size(208, 20)
+        Me.cuotas.TabIndex = 2
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(120, 131)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(57, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "descuento"
+        '
+        'descuento
+        '
+        Me.descuento.Location = New System.Drawing.Point(38, 147)
+        Me.descuento.Name = "descuento"
+        Me.descuento.Size = New System.Drawing.Size(208, 20)
+        Me.descuento.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(122, 170)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(55, 13)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "montofinal"
+        '
+        'montofinal
+        '
+        Me.montofinal.Location = New System.Drawing.Point(38, 186)
+        Me.montofinal.Name = "montofinal"
+        Me.montofinal.Size = New System.Drawing.Size(208, 20)
+        Me.montofinal.TabIndex = 6
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(120, 214)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(78, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "cuotasmensual"
+        '
+        'cuotamensual
+        '
+        Me.cuotamensual.Location = New System.Drawing.Point(38, 229)
+        Me.cuotamensual.Name = "cuotamensual"
+        Me.cuotamensual.Size = New System.Drawing.Size(208, 20)
+        Me.cuotamensual.TabIndex = 8
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(107, 99)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "calcular"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.cuotamensual)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.montofinal)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.descuento)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cuotas)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.monto)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents monto As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents cuotas As System.Windows.Forms.TextBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents descuento As System.Windows.Forms.TextBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents montofinal As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents cuotamensual As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+
+End Class
---
+++ And primer ejercicio/Form1.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        If monto.Text > 10000 And cuotas.Text < 6 Then
+            descuento.Text = monto.Text * 0.3
+            montofinal.Text = monto.Text - descuento.Text
+            cuotamensual.Text = montofinal.Text / cuotas.Text
+        Else
+            descuento.Text = 800
+            montofinal.Text = monto.Text - descuento.Text
+            cuotamensual.Text = montofinal.Text / cuotas.Text
+        End If
+    End Sub
+End Class
